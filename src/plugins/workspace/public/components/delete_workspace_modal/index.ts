c49e212212e7c5490f4caea171e9416cbc3cb91f
yuye-aws <yuyezhu@amazon.com>
2023-06-20 12:01:46 +0800

    add delete workspace modal
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

---
+++ src/plugins/workspace/public/components/delete_workspace_modal/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import DeleteWorkspaceModal from './delete_workspace_modal';
+
+// eslint-disable-next-line import/no-default-export
+export default DeleteWorkspaceModal;
