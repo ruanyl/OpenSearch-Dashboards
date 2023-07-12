33226ab26bcf7db3dfc7615736c5a723b334fe81
Yulong Ruan <ruanyl@amazon.com>
2023-07-12 17:26:42 +0800

    allow user to turn on/off workspace from advance settings
    navigate to basePath if user open workspace url while workspace is
    disabled
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- src/core/public/workspace/workspaces_service.ts
+++ src/core/public/workspace/workspaces_service.ts
@@ -6,6 +6,8 @@ import { CoreService } from 'src/core/types';
 import { WorkspacesClient, WorkspacesClientContract } from './workspaces_client';
 import type { WorkspaceAttribute } from '../../server/types';
 import { HttpSetup } from '../http';
+import { IUiSettingsClient } from '../ui_settings';
+import { getWorkspaceIdFromUrl } from '../utils/workspace';
 
 /**
  * @public
@@ -26,7 +28,15 @@ export class WorkspacesService implements CoreService<WorkspacesSetup, Workspace
   private setFormatUrlWithWorkspaceId(formatFn: WorkspacesStart['formatUrlWithWorkspaceId']) {
     this.formatUrlWithWorkspaceId = formatFn;
   }
-  public async setup({ http }: { http: HttpSetup }) {
+  public async setup({ http, uiSettings }: { http: HttpSetup; uiSettings: IUiSettingsClient }) {
+    // If workspace was disabled while opening a workspace url, navigate to basePath
+    if (uiSettings.get('workspace:enabled') === false) {
+      const workspaceId = getWorkspaceIdFromUrl(window.location.href);
+      if (workspaceId) {
+        window.location.href = http.basePath.getBasePath();
+      }
+    }
+
     this.client = new WorkspacesClient(http);
     return {
       client: this.client,
