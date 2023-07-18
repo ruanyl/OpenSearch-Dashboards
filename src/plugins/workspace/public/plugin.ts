f65331ed89100ec43754ab31bc53dd5c01e25a00
yuye-aws <yuyezhu@amazon.com>
2023-07-18 15:20:04 +0800

    Check workspace enabled for left menu
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/plugins/workspace/public/plugin.ts
+++ src/plugins/workspace/public/plugin.ts
@@ -148,14 +148,32 @@ export class WorkspacesPlugin implements Plugin<{}, {}, WorkspacesPluginSetupDep
   }
 
   public start(core: CoreStart) {
-    mountDropdownList(core);
-    this._changeSavedObjectCurrentWorkspace();
+    // If workspace feature is disabled, it will not load the workspace plugin
+    if (core.uiSettings.get('workspace:enabled') === false) {
+      // need to set filtered nav links
+      const navLinksService = core.chrome.navLinks;
+      const chromeNavLinks$ = navLinksService.getNavLinks$();
+      combineLatest([chromeNavLinks$]).subscribe(([chromeNavLinks]) => {
+        const filteredNavLinks = new Map<string, ChromeNavLink>();
+        chromeNavLinks.forEach((chromeNavLink) =>
+          filteredNavLinks.set(chromeNavLink.id, chromeNavLink)
+        );
+        navLinksService.setFilteredNavLinks(filteredNavLinks);
+      });
+      return {};
+    }
+    this.coreStart = core;
+    mountDropdownList({
+      application: core.application,
+      workspaces: core.workspaces,
+      chrome: core.chrome,
+    });
+    this.currentWorkspaceSubscription = this._changeSavedObjectCurrentWorkspace();
     if (core) {
       const navLinksService = core.chrome.navLinks;
       const chromeNavLinks$ = navLinksService.getNavLinks$();
       const workspaceList$ = core.workspaces.client.workspaceList$;
       const currentWorkspace$ = core.workspaces.client.currentWorkspace$;
-      // const workspaceEnabled: boolean = core.uiSettings.get('workspace:enabled');
       combineLatest([workspaceList$, chromeNavLinks$, currentWorkspace$]).subscribe(
         ([workspaceList, chromeNavLinks, currentWorkspace]) => {
           const filteredNavLinks = new Map<string, ChromeNavLink>();
