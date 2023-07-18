b63faed08c14c51fdf744b93f379d4b295b9f9ad
yuye-aws <yuyezhu@amazon.com>
2023-07-18 10:54:27 +0800

    Add euiIconType for workspace left menu category
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/plugins/workspace/common/constants.ts
+++ src/plugins/workspace/common/constants.ts
@@ -4,6 +4,7 @@
  */
 
 import { i18n } from '@osd/i18n';
+import { AppCategory } from '../../../core/types';
 
 export const WORKSPACE_APP_ID = 'workspace';
 export const WORKSPACE_APP_NAME = 'Workspace';
@@ -17,10 +18,11 @@ export const PATHS = {
 export const WORKSPACE_OP_TYPE_CREATE = 'create';
 export const WORKSPACE_OP_TYPE_UPDATE = 'update';
 
-export const WORKSPACE_NAV_CATEGORY = {
+export const WORKSPACE_NAV_CATEGORY: AppCategory = {
   id: 'workspace',
   label: i18n.translate('core.ui.workspaceNavList.label', {
     defaultMessage: 'Workspaces',
   }),
+  euiIconType: 'folderClosed',
   order: 2000,
 };
