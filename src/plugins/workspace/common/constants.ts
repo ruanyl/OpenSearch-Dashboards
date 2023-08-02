316ecf3f494170bd82420a02e4579764407fdc36
yuye-aws <yuyezhu@amazon.com>
2023-08-02 13:19:02 +0800

    remove createbreadcrumb for workspace apps
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/plugins/workspace/common/constants.ts
+++ src/plugins/workspace/common/constants.ts
@@ -10,8 +10,6 @@ export const WORKSPACE_CREATE_APP_ID = 'workspace_create';
 export const WORKSPACE_LIST_APP_ID = 'workspace_list';
 export const WORKSPACE_UPDATE_APP_ID = 'workspace_update';
 export const WORKSPACE_OVERVIEW_APP_ID = 'workspace_overview';
-export const WORKSPACE_APP_NAME = 'Workspace';
-
 export const PATHS = {
   create: '/create',
   overview: '/overview',
