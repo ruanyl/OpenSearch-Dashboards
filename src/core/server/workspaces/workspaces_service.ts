e09a8870369a9008f1e06423c4d0637b4d747e78
SuZhou-Joe <suzhou@amazon.com>
2023-08-03 13:21:48 +0800

    feat: create public workspace when service start
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/core/server/workspaces/workspaces_service.ts
+++ src/core/server/workspaces/workspaces_service.ts
@@ -8,12 +8,14 @@ import { CoreContext } from '../core_context';
 import { InternalHttpServiceSetup } from '../http';
 import { Logger } from '../logging';
 import { registerRoutes } from './routes';
-import { InternalSavedObjectsServiceSetup } from '../saved_objects';
-import { IWorkspaceDBImpl } from './types';
+import { InternalSavedObjectsServiceSetup, SavedObjectsServiceStart } from '../saved_objects';
+import { IWorkspaceDBImpl, WorkspaceAttribute } from './types';
 import { WorkspacesClientWithSavedObject } from './workspaces_client';
 import { WorkspaceSavedObjectsClientWrapper } from './saved_objects';
 import { InternalUiSettingsServiceSetup } from '../ui_settings';
 import { uiSettings } from './ui_settings';
+import { WORKSPACE_TYPE } from './constants';
+import { PUBLIC_WORKSPACE } from '../../utils';
 
 export interface WorkspacesServiceSetup {
   client: IWorkspaceDBImpl;
@@ -27,6 +29,10 @@ export interface WorkspacesSetupDeps {
   http: InternalHttpServiceSetup;
   savedObject: InternalSavedObjectsServiceSetup;
   uiSettings: InternalUiSettingsServiceSetup;
+}
+
+export interface WorkpsaceStartDeps {
+  savedObjects: SavedObjectsServiceStart;
 }
 
 export type InternalWorkspacesServiceSetup = WorkspacesServiceSetup;
@@ -89,8 +95,35 @@ export class WorkspacesService
     };
   }
 
-  public async start(): Promise<InternalWorkspacesServiceStart> {
+  public async start(startDeps: WorkpsaceStartDeps): Promise<InternalWorkspacesServiceStart> {
     this.logger.debug('Starting SavedObjects service');
+    /**
+     * Internal repository is attached to global tenant.
+     */
+    const internalRepository = startDeps.savedObjects.createInternalRepository();
+
+    try {
+      await internalRepository.get(WORKSPACE_TYPE, PUBLIC_WORKSPACE);
+    } catch (error) {
+      this.logger.debug(error?.toString() || '');
+      this.logger.info('No public workspace found, create it by using internal user');
+      try {
+        const createResult = await internalRepository.create(
+          WORKSPACE_TYPE,
+          {
+            name: 'public',
+          } as Omit<WorkspaceAttribute, 'id'>,
+          {
+            id: PUBLIC_WORKSPACE,
+          }
+        );
+        if (createResult.id) {
+          this.logger.info(`Created workspace ${createResult.id} in global tenant.`);
+        }
+      } catch (e) {
+        this.logger.error(`Create public workspace error: ${e?.toString() || ''}`);
+      }
+    }
 
     return {
       client: this.client as IWorkspaceDBImpl,
