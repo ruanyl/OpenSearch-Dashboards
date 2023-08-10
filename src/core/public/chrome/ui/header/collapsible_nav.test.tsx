a341f0868aea91a9879c96aa4069507f84ff0150
SuZhou-Joe <suzhou@amazon.com>
2023-08-10 16:07:02 +0800

    feat: revert some skipped test
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/core/public/chrome/ui/header/collapsible_nav.test.tsx
+++ src/core/public/chrome/ui/header/collapsible_nav.test.tsx
@@ -152,10 +152,6 @@ describe('CollapsibleNav', () => {
   });
 
   it('remembers collapsible section state', () => {
-    /**
-     * TODO skip for workspace refractor, will revert once refractor the left menu part
-     */
-    return;
     const navLinks = [
       mockLink({ category: opensearchDashboards }),
       mockLink({ category: observability }),
@@ -169,7 +165,7 @@ describe('CollapsibleNav', () => {
         recentlyAccessed$={new BehaviorSubject(recentNavLinks)}
       />
     );
-    expectShownNavLinksCount(component, 0);
+    expectShownNavLinksCount(component, 3);
     clickGroup(component, 'opensearchDashboards');
     clickGroup(component, 'recentlyViewed');
     expectShownNavLinksCount(component, 1);
@@ -180,10 +176,6 @@ describe('CollapsibleNav', () => {
   });
 
   it('closes the nav after clicking a link', () => {
-    /**
-     * TODO skip for workspace refractor, will revert once refractor the left menu part
-     */
-    return;
     const onClose = sinon.spy();
     const navLinks = [
       mockLink({ category: opensearchDashboards }),
