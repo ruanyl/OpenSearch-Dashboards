7682ba11ec0d656b37f43a1a7c44f5acac0cff72
SuZhou-Joe <suzhou@amazon.com>
2023-08-15 13:45:09 +0800

    fix: type check
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/core/public/workspace/workspaces_service.mock.ts
+++ src/core/public/workspace/workspaces_service.mock.ts
@@ -9,11 +9,13 @@ import { WorkspaceAttribute } from '../workspace';
 const currentWorkspaceId$ = new BehaviorSubject<string>('');
 const workspaceList$ = new BehaviorSubject<WorkspaceAttribute[]>([]);
 const currentWorkspace$ = new BehaviorSubject<WorkspaceAttribute | null>(null);
+const hasFetchedWorkspaceList$ = new BehaviorSubject<boolean>(false);
 
 const createWorkspacesSetupContractMock = () => ({
   currentWorkspaceId$,
   workspaceList$,
   currentWorkspace$,
+  hasFetchedWorkspaceList$,
 });
 
 const createWorkspacesStartContractMock = createWorkspacesSetupContractMock;
