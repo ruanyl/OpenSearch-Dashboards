eb53b14d9a706b77222d345af67a5cb5a72ce381
Yulong Ruan <ruanyl@amazon.com>
2023-08-22 07:50:51 +0800

    add missing licence header
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- src/plugins/workspace/public/utils.test.ts
+++ src/plugins/workspace/public/utils.test.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { featureMatchesConfig } from './utils';
 
 describe('workspace utils: featureMatchesConfig', () => {
