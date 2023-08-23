f710e132614804e726f7082a1157958959c84ecb
Yulong Ruan <ruanyl@amazon.com>
2023-08-23 14:46:10 +0800

    change workspace id size to 6
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

---
+++ src/plugins/workspace/server/utils.ts
@@ -0,0 +1,8 @@
+import crypto from 'crypto';
+
+/**
+ * Generate URL friendly random ID
+ */
+export const generateRandomId = (size: number) => {
+  return crypto.randomBytes(size).toString('base64url').slice(0, size);
+};
