c2e9435c58514309cbce1c36ca9bfa93321dcfd2
SuZhou-Joe <suzhou@amazon.com>
2024-02-27 17:17:10 +0800

    fix: hiddenType side effect
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/server/plugin.ts
+++ src/plugins/workspace/server/plugin.ts
@@ -31,6 +31,7 @@ export class WorkspacePlugin implements Plugin<{}, {}> {
   private client?: IWorkspaceClientImpl;
   private permissionControl?: SavedObjectsPermissionControlContract;
   private readonly config$: Observable<ConfigSchema>;
+  private workspaceSavedObjectsClientWrapper?: WorkspaceSavedObjectsClientWrapper;
 
   private proxyWorkspaceTrafficToRealHandler(setupDeps: CoreSetup) {
     /**
@@ -72,14 +73,14 @@ export class WorkspacePlugin implements Plugin<{}, {}> {
         permissionControl: this.permissionControl,
       });
 
-      const workspaceSavedObjectsClientWrapper = new WorkspaceSavedObjectsClientWrapper(
+      this.workspaceSavedObjectsClientWrapper = new WorkspaceSavedObjectsClientWrapper(
         this.permissionControl
       );
 
       core.savedObjects.addClientWrapper(
         0,
         WORKSPACE_SAVED_OBJECTS_CLIENT_WRAPPER_ID,
-        workspaceSavedObjectsClientWrapper.wrapperFactory
+        this.workspaceSavedObjectsClientWrapper.wrapperFactory
       );
     }
 
@@ -93,11 +94,7 @@ export class WorkspacePlugin implements Plugin<{}, {}> {
 
     core.savedObjects.setClientFactoryProvider(
       (repositoryFactory) => ({ includedHiddenTypes }: { includedHiddenTypes?: string[] }) =>
-        new SavedObjectsClient(
-          repositoryFactory.createInternalRepository([
-            ...new Set([WORKSPACE_TYPE, ...(includedHiddenTypes || [])]),
-          ])
-        )
+        new SavedObjectsClient(repositoryFactory.createInternalRepository(includedHiddenTypes))
     );
 
     core.capabilities.registerProvider(() => ({
@@ -116,6 +113,7 @@ export class WorkspacePlugin implements Plugin<{}, {}> {
     this.logger.debug('Starting SavedObjects service');
     this.permissionControl?.setup(core.savedObjects.getScopedClient);
     this.client?.setSavedObjects(core.savedObjects);
+    this.workspaceSavedObjectsClientWrapper?.setScopedClient(core.savedObjects.getScopedClient);
 
     return {
       client: this.client as IWorkspaceClientImpl,
